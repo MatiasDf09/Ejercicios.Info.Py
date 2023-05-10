66a7eca30701f1887559161b63f6957443f4e239
MatiasDf <matiasdf9@gmail.com>
2023-05-09 21:20:13 -0300

    Subo el Ejercicio 1 De Control Loops

---
+++ Ejercicos Listas/Ejer1_CL.py
@@ -0,0 +1,9 @@
+#    1-Escribe un programa que pida al usuario una palabra y luego imprima cada
+#    letra de la palabra en una línea separada. 
+
+palabra = input("Ingrese una Palabra: ")
+for i in palabra:
+    print (i) 
+
+#  Usamos "i" Por cada elemento de la estructura 
+
