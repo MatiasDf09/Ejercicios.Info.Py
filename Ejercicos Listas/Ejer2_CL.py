016b4bce5efa53c5c1869bd949b289fc78d304c1
MatiasDf <matiasdf9@gmail.com>
2023-05-09 22:02:45 -0300

    Subo el ejercicio 2 de Control Loops

--- Ejercicos Listas/Ejer2_CL.py
+++ Ejercicos Listas/Ejer2_CL.py
@@ -1,9 +1,11 @@
 # 2-Escribe un programa que pida al usuario un número y calcule la suma de todos
-# los números naturales del 1 hasta ese número. 
-
+# los números naturales del 1 hasta ese número.
+ 
+# Pido el numero al usurio 
 num = int(input("Ingrese un numero: "))
 calculo = 0
-for i in range(1, num + 1, 1):
-    calculo = calculo + i
-    
+for i in range(1, num + 1, 1): # Bucle "For" que inicializa en 1 va hasta el n dado y va en 1 en 1 
+    calculo = calculo + i      
+   
+ #  Mostramos el calculo aqui    
 print (calculo)
